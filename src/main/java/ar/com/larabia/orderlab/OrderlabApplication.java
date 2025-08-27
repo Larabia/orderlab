e099692283aad07a611ead04ba574e6e6c7c70aa
lorena <lorena.arabia@fluxit.com.ar>
2025-08-27 12:14:11 -0300

    chore(app): bootstrap Spring Boot (Web + Actuator), Java 21

---
+++ src/main/java/ar/com/larabia/orderlab/OrderlabApplication.java
@@ -0,0 +1,13 @@
+package ar.com.larabia.orderlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/larabia/orderlab/OrderlabApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.larabia.orderlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
